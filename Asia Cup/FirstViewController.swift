05daaa9f59b50a2df7e3af3af62a37f98d530bdd
Daniel James <daniel.james@zohocorp.com>
2018-06-27 06:46:40 +0530

    Initial Commit

---
+++ Asia Cup/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Asia Cup
+//
+//  Created by Daniel James on 27/06/18.
+//  Copyright © 2018 Daniel James. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
